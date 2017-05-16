4f75666111652cd3c8edfb89f6602f5117a2ab22
Sidd Seethepalli <siddseethepalli@gmail.com>
2017-05-16 04:32:20 -0400

    Create multi disc repulsion GAN

--- tutorial/multi_disc_wgan.py
+++ tutorial/multi_disc_wgan.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+import os
 from collections import defaultdict
 try:
     import cPickle as pickle
@@ -13,21 +14,36 @@ from six.moves import range
 
 import keras.backend as K
 from keras.datasets import mnist
-from keras.layers import Input, Dense, Reshape, Flatten, Embedding, merge, Dropout
+from keras.layers import Input, Dense, Reshape, Flatten, Embedding, Dropout, average, concatenate, Lambda, add, dot, multiply
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Convolution2D
+from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.layers.noise import GaussianNoise
 from keras.models import Sequential, Model
 from keras.optimizers import Adam, SGD
 from keras.backend.common import _EPSILON
 from keras.utils.generic_utils import Progbar
 import numpy as np
+import tensorflow as tf
 
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 np.random.seed(1331)
 K.set_image_dim_ordering('th')
 
-def modified_binary_crossentropy(target, output):
+NUM_DISCRIMINATORS = 3
+
+def l2norm(a, b):
+    return dot([a, b], 0)
+
+def modified_binary_crossentropy_disc(target, output):
     return K.mean(target*output)
+
+def modified_binary_crossentropy_super(target, output):
+    repel_error = K.constant(0)
+    for i in range(NUM_DISCRIMINATORS):
+        for j in range(i + 1, NUM_DISCRIMINATORS):
+            s = Reshape((-1, 1))(output[:, i] - output[:, j])
+            repel_error += 1 / l2norm(s, s)
+    return K.mean(target*output) + repel_error
 
 def build_generator(latent_size):
     # we will map a pair of (z, L), where z is a latent vector and L is a
@@ -42,19 +58,16 @@ def build_generator(latent_size):
 
     # upsample to (..., 14, 14)
     cnn.add(UpSampling2D(size=(2, 2)))
-    cnn.add(Convolution2D(256, 5, 5, border_mode='same',
-                          init='glorot_uniform'))
+    cnn.add(Conv2D(256, (5, 5), padding='same', kernel_initializer='glorot_uniform'))
     cnn.add(LeakyReLU())
 
     # upsample to (..., 28, 28)
     cnn.add(UpSampling2D(size=(2, 2)))
-    cnn.add(Convolution2D(128, 5, 5, border_mode='same',
-                          init='glorot_uniform'))
+    cnn.add(Conv2D(128, (5, 5), padding='same', kernel_initializer='glorot_uniform'))
     cnn.add(LeakyReLU())
 
     # take a channel axis reduction
-    cnn.add(Convolution2D(1, 2, 2, border_mode='same',
-                          activation='tanh', init='glorot_uniform'))
+    cnn.add(Conv2D(1, (2, 2), padding='same', activation='tanh', kernel_initializer='glorot_uniform'))
 
     # this is the z space commonly refered to in GAN papers
     latent = Input(shape=(latent_size, ))
@@ -63,35 +76,33 @@ def build_generator(latent_size):
     image_class = Input(shape=(1,), dtype='int32')
 
     # 10 classes in MNIST
-    cls = Flatten()(Embedding(10, latent_size,
-                              init='glorot_uniform')(image_class))
+    cls = Flatten()(Embedding(10, 100, embeddings_initializer="glorot_uniform")(image_class))
 
     # hadamard product between z-space and a class conditional embedding
-    h = merge([latent, cls], mode='mul')
+    h = multiply([latent, cls])
 
     fake_image = cnn(h)
 
-    return Model(input=[latent, image_class], output=fake_image)
+    return Model(inputs=[latent, image_class], outputs=fake_image)
 
 def build_discriminator():
     # build a relatively standard conv net, with LeakyReLUs as suggested in
     # the reference paper
     cnn = Sequential()
     #cnn.add(GaussianNoise(0.2, input_shape=(1, 28, 28)))
-    cnn.add(Convolution2D(32, 3, 3, border_mode='same', subsample=(2, 2),
-                          input_shape=(1, 28, 28)))
+    cnn.add(Conv2D(32, (3, 3), padding="same", strides=(2, 2), input_shape=(1, 28, 28)))
     cnn.add(LeakyReLU())
     cnn.add(Dropout(0.3))
 
-    cnn.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
+    cnn.add(Conv2D(64, (3, 3), padding='same', strides=(1, 1)))
     cnn.add(LeakyReLU())
     cnn.add(Dropout(0.3))
 
-    cnn.add(Convolution2D(128, 3, 3, border_mode='same', subsample=(2, 2)))
+    cnn.add(Conv2D(128, (3, 3), padding='same', strides=(2, 2)))
     cnn.add(LeakyReLU())
     cnn.add(Dropout(0.3))
 
-    cnn.add(Convolution2D(256, 3, 3, border_mode='same', subsample=(1, 1)))
+    cnn.add(Conv2D(256, (3, 3), padding='same', strides=(1, 1)))
     cnn.add(LeakyReLU())
     cnn.add(Dropout(0.3))
 
@@ -108,24 +119,61 @@ def build_discriminator():
     fake = Dense(1, activation='linear', name='generation')(features)
     aux = Dense(10, activation='softmax', name='auxiliary')(features)
 
-    return Model(input=image, output=[fake, aux])
+    return Model(inputs=image, outputs=[fake, aux])
+
+def build_super_discriminator(discriminators):
+    image = Input(shape=(1, 28, 28))
+
+    fakes = []
+    auxes = []
+    for discriminator in discriminators:
+        d_fake, d_aux = discriminator(image)
+        fakes.append(Reshape((-1,))(d_fake))
+        auxes.append(d_aux)
+
+    fake = concatenate(fakes)
+    # out = []
+    # for i in range(256):
+    #     out.append(tf.strided_slice(fake,i,2*256+i,256))
+    # fake = tf.stack(out)
+    aux = average(auxes)
+    
+    # def l2norm(a, b):
+    #     return Reshape(())(dot([a - b, a - b], 0))
+
+    # diffs = []
+    # for i in range(len(discriminators)):
+    #     for j in range(i + 1, len(discriminators)):
+    #         diffs.append(l2norm(fakes[i], fakes[j]))
+
+    # diff = concatenate(diffs)
+
+    return Model(inputs=image, outputs=[fake, aux])
 
 def train():
     # batch and latent size taken from the paper
-    nb_batches = 1000000
+    nb_batches = 100000
     batch_size = 128
+    epoch_size = 20
     latent_size = 100
 
     # Adam parameters suggested in https://arxiv.org/abs/1511.06434
     adam_lr = 0.0002
     adam_beta_1 = 0.5
 
-    # build the discriminator
-    discriminator = build_discriminator()
-    discriminator.compile(
-        optimizer=SGD(clipvalue=0.01),#Adam(lr=adam_lr, beta_1=adam_beta_1),
-        loss=[modified_binary_crossentropy, 'sparse_categorical_crossentropy']
-    )
+    # build the super discriminator
+    discriminators = []
+    for _ in range(NUM_DISCRIMINATORS):
+        discriminator = build_discriminator()
+        discriminator.compile(
+            optimizer=SGD(clipvalue=0.01),#Adam(lr=adam_lr, beta_1=adam_beta_1),
+            loss=[modified_binary_crossentropy_disc, 'sparse_categorical_crossentropy']
+        )
+        discriminators.append(discriminator)
+
+    super_discriminator = build_super_discriminator(discriminators)
+    super_discriminator.compile(optimizer=SGD(clipvalue=0.01),
+            loss=[modified_binary_crossentropy_super, 'sparse_categorical_crossentropy'])
 
     # build the generator
     generator = build_generator(latent_size)
@@ -139,14 +187,14 @@ def train():
     fake = generator([latent, image_class])
 
     # we only want to be able to train generation for the combined model
-    discriminator.trainable = False
-    fake, aux = discriminator(fake)
-    combined = Model(input=[latent, image_class], output=[fake, aux])
+    super_discriminator.trainable = False
+    fakes, aux = super_discriminator(fake)
+    combined = Model(inputs=[latent, image_class], outputs=[fakes, aux])
 
     combined.compile(
         optimizer='RMSprop',
-        loss=[modified_binary_crossentropy, 'sparse_categorical_crossentropy']
-    )
+        loss=[modified_binary_crossentropy_disc,
+              'sparse_categorical_crossentropy'])
 
     # get our mnist data, and force it to be of shape (..., 1, 28, 28) with
     # range [-1, 1]
@@ -175,24 +223,24 @@ def train():
             [noise, sampled_labels.reshape((-1, 1))], verbose=0)
 
         X = np.concatenate((image_batch, generated_images))
-        y = np.array([-1] * batch_size + [1] * batch_size)
+        y = np.array([-1] * batch_size * NUM_DISCRIMINATORS + [1] * batch_size * NUM_DISCRIMINATORS).reshape((2 * batch_size, NUM_DISCRIMINATORS))
         aux_y = np.concatenate((label_batch, sampled_labels), axis=0)
 
-        disc_loss = discriminator.train_on_batch(X, [y, aux_y])
+        disc_loss = super_discriminator.train_on_batch(X, [y, aux_y])
 
         noise = np.random.normal(0, 1, (2 * batch_size, latent_size))
         sampled_labels = np.random.randint(0, 10, 2 * batch_size)
-        trick = -np.ones(2 * batch_size)
+        trick = -np.ones(2 * batch_size * NUM_DISCRIMINATORS).reshape((2 * batch_size, NUM_DISCRIMINATORS))
 
         gen_loss = combined.train_on_batch(
             [noise, sampled_labels.reshape((-1, 1))], [trick, sampled_labels])
 
         print('disc_loss: {} gen_loss: {}'.format(disc_loss, gen_loss))
 
-        if batch % 100 == 99:
+        if batch % epoch_size == epoch_size - 1:
             generator.save_weights(
                 'params_generator_batch_{0:06d}.hdf5'.format(batch), True)
-            discriminator.save_weights(
+            super_discriminator.save_weights(
                 'params_discriminator_batch_{0:06d}.hdf5'.format(batch), True)
 
             noise = np.random.normal(-1, 1, (100, latent_size))
